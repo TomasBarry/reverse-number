47fdd21167da05df3e8e11a7252a99a600eb98fd
Tomas Barry <tomas.barry@hotmail.com>
2017-02-08 16:35:14 +0000

    Tested and README filled out

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # reverse-number
 Reverse a number without having to do any conversions!
+
+## Installation
+To install, run ```npm install reverse-number --save```
+
+## Usage
+The following snippet shows just how easily you can reverse a number:
+```javascript
+const reverse = require('reverse-number');
+
+const my_number = 1234; // The number to be reversed
+
+const my_number_reversed = reverse(my_number); // The reverse of your number
+
+console.log(`${my_number} reversed is ${my_number_reversed}`); // Prints 1234 reversed is 4321
+```
+
+## Notes
+1. Numbers must be positive
+2. Numbers must be less than Number.MAX_SAFE_INTEGER
--- test/reverse.js
+++ test/reverse.js
@@ -1,13 +1,32 @@
-const reverse = require('../reverse.js);
+const chai = require('chai');
+const expect = chai.expect;
+
+const reverse = require('../reverse.js');
 
 describe('Reverse', () => {
-  describe('Numbers', () => {
+  describe('Valid Numbers', () => {
     it('should correctly reverse numbers', (done) => {
       expect(reverse(0)).to.equal(0);
       expect(reverse(1)).to.equal(1);
+      expect(reverse(10)).to.equal(1);
       expect(reverse(10)).to.equal(01);
       expect(reverse(101)).to.equal(101);
       expect(reverse(4321)).to.equal(1234);
+      done();
+    });
+  });
+
+  describe('Invalid Numbers', () => {
+    it('should correctly throw errors', (done) => {
+      expect(() => reverse(-1)).to.throw('-1 is less than 0');
+      expect(() => reverse(Number.MAX_SAFE_INTEGER + 1)).to.throw(`${Number.MAX_SAFE_INTEGER + 1} is greater than ${Number.MAX_SAFE_INTEGER} which is the maximum safe integer`);
+      done();
+    });
+  });
+
+  describe('Not Numbers', () => {
+    it('should correctly throw errors', (done) => {
+      expect(() => reverse('hello')).to.throw('hello is not a number');
       done();
     });
   });
